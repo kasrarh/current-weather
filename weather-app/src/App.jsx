6bfbf1e5400c11b296456f813f06e8dbe1b75723
kasrarh <55912648+kasrarh@users.noreply.github.com>
2024-05-20 21:35:41 -0400

    Update App.jsx

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { WeatherContainer } from './Pages/WeatherContainer';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from './Pages/Home';
